4a350afd578c38e9ebb16c9286226cf45b439c86
Martynas <alijosius.martynas@gmail.com>
2021-02-09 00:02:45 +0200

    pages/add-post/validation

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,11 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class BlogController extends Controller
 {
     public function index()
     {
-        return view('blog_theme.pages.home');
+        $posts = Post::paginate(5);
+        return view('blog_theme.pages.home', compact('posts'));
+    }
+
+
+    public function addPost(){
+
+        return view('blog_theme.pages.add-post');
+    }
+
+    public function store(Request  $request){
+        $validatedData = $request->validate([
+            'title' => 'required|unique:posts|max:255',
+            'body' => 'required',
+            'category' => 'required'
+        ]);
+
+        Post::create([
+            'title' => request('title'),
+            'category' => request('category'),
+            'body' => request('body')
+        ]);
+
+        return redirect('/');
     }
 }
--- resources/views/blog_theme/pages/add-post.blade.php
+++ resources/views/blog_theme/pages/add-post.blade.php
@@ -1,2 +1,34 @@
-<?php
 
+@extends('blog_theme/main')
+@section('content')
+    <div class="row justify-content-center mb-5">
+        <h2>New Post</h2>
+    </div>
+    @include('blog_theme/_partials/errors')
+    <form action="/store" method="post">
+        {{csrf_field()}}
+    <div class="input-group">
+        <input name="title" type="text" placeholder="Name.." class="form-control" aria-label="Text input with dropdown button" >
+        <div class="input-group-append"></div>
+    </div>
+        <select name="category" class="mt-3 form-control form-control-lg">
+            <option value="Music">Music</option>
+            <option value="Films">Films</option>
+            <option value="Sport">Sport</option>
+        </select>
+
+
+
+        <div class="mt-4 form-group">
+            <textarea name="body" placeholder="Write your post here.." class="form-control" id="content" rows="5"></textarea>
+        </div>
+
+        <div class="form-group">
+            <input name="img" type="file"  class="form-control" id="upload">
+        </div>
+
+        <div class="form-group d-flex justify-content-center m-5">
+            <button type="submit" class="btn btn-secondary">Post</button>
+        </div>
+    </form>
+@endsection
